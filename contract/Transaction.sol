ae95713463db80c474e25d7f54000640d11d4dd8
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-20 11:52:38 -0600

    Add & Apply Modifiers

--- contract/Transaction.sol
+++ contract/Transaction.sol
@@ -5,7 +5,23 @@ contract Transaction {
     event SenderLogger(address);
     event ValueLogger(uint);
 
-    function () payable {
+    address private owner;
+
+    modifier isOwner {
+        require(owner == msg.sender);
+        _;
+    }
+
+    modifier validValue {
+        assert(msg.value >= 1 ether);
+        _;
+    }
+
+    function Trasaction() {
+        owner = msg.sender;
+    }
+
+    function () payable isOwner validValue {
         SenderLogger(msg.sender);
         ValueLogger(msg.value);
     }
