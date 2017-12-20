33b2b2ee070f7d9c2155694784c06cbf19e82313
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-20 15:52:05 -0600

    Add transferFrom, approve, & allowance functions to Token Contract

--- Token/Token.sol
+++ Token/Token.sol
@@ -10,6 +10,11 @@ contract Token is ERC20 {
     uint private constant __totalSupply = 1000;
 
     mapping (address => uint) private __balanceOf;
+    mapping (address => mapping (address => uint)) private __allowances;
+
+    function Token() {
+        __balanceOf[msg.sender] = __totalSupply;
+    }
 
     function totalSupply() constant returns (uint _totalSupply) {
         _totalSupply == __totalSupply;
@@ -26,5 +31,25 @@ contract Token is ERC20 {
             return true;
         }
         return false;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) returns (bool success) {
+        if (__allowances[_from][msg.sender] > 0 &&
+            _value > 0 &&
+            __allowances[_from][msg.sender] >= _value) {
+                __balanceOf[_from] -= _value;
+                __balanceOf[_to] += _value;
+                return true;
+            }
+            return false;
+    }
+
+    function approve(address _spender, uint _value) returns (bool success) {
+        __allowances[msg.sender][_spender] = _value;
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint remaining) {
+        return __allowances[_owner][_spender];
     }
 }
