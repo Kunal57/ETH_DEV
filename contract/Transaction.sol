0b2583fe3c58b5abf95cf960971e67b941b23d00
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-20 11:45:44 -0600

    Setup Event Handlers for sender & value

--- contract/Transaction.sol
+++ contract/Transaction.sol
@@ -2,9 +2,12 @@ pragma solidity ^0.4.17;
 
 contract Transaction {
 
+    event SenderLogger(address);
+    event ValueLogger(uint);
 
     function () payable {
-
+        SenderLogger(msg.sender);
+        ValueLogger(msg.value);
     }
 
 }
