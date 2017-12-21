f1fc2016941b2d810427364369c77ccfacaf1b2a
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-21 12:07:31 -0600

    Create basic Lease Functionality

---
+++ lease/Lease.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.17;
+
+contract TimeBased {
+
+    mapping(address => uint) public _balanceOf;
+    mapping(address => uint) public _expiryOf;
+
+    uint private leaseTime = 600;
+
+    function lease()
+        public
+        payable
+        returns (bool) {
+        require(msg.value == 1 ether);
+        require(_balanceOf[msg.sender] == 0);
+        _balanceOf[msg.sender] = 1;
+        _expiryOf[msg.sender] = block.timestamp + leaseTime;
+        return true;
+    }
+
+    function balanceOf()
+        public
+        returns (uint) {
+        return balanceOf(msg.sender);
+    }
+
+    function balanceOf(address _addr)
+        public
+        returns (uint) {
+        return _balanceOf[_addr];
+    }
+
+    function expiryOf()
+        public
+        returns (uint) {
+        return expiryOf(msg.sender);
+    }
+
+    function expiryOf(address _addr)
+        public
+        returns (uint) {
+        return _expiryOf[_addr];
+    }
+
+}
