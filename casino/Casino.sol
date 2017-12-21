fa29a6e4a749dd5aa8d86414556b9d7f9c27e2d5
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-21 17:07:22 -0600

    Add verifyTicket, checkWinner, claim functions

--- casino/Casino.sol
+++ casino/Casino.sol
@@ -6,11 +6,17 @@ contract Casino {
 
     uint private buyPeriod = 1000;
     uint private verifyPeriod = 100;
-    uint private claimPeriod = 100;
+    uint private checkPeriod = 100;
 
-    mapping (address => uint) _tickets;
+    mapping (address => uint) private _tickets;
+    mapping (address => uint) private _winnings;
     address[] _entries;
     address[] _verified;
+
+
+    uint private winnerSeed;
+    bool private hasWinner;
+    address private winner;
 
     function Casino()
         public {
@@ -18,12 +24,19 @@ contract Casino {
     }
 
     // This function should NOT be a part of the contract
-    function generateHash(uint number, uint salt)
+    function unsafeEntry(uint number, uint salt)
         public
         payable
         returns (bool) {
-        return buyTicket(uint(keccak256(number + salt)));
+        return buyTicket(generateHash(number, salt));
     }
+
+    function generateHash(uint number, uint salt)
+        public
+        pure
+        returns (uint) {
+            return uint(keccak256(number + salt));
+        }
 
     function buyTicket(uint hash)
         public
@@ -37,6 +50,43 @@ contract Casino {
         require(_tickets[msg.sender] == 0);
         _tickets[msg.sender] = hash;
         _entries.push(msg.sender);
+        return true;
     }
 
+    function verifyTicket(uint number, uint salt)
+        public
+        returns (bool) {
+        // Within the timeframe
+        require(block.timestamp < start + buyPeriod);
+        require(block.timestamp < start + buyPeriod + verifyPeriod);
+        // Has a valid entry
+        require(_tickets[msg.sender] > 0);
+        // Validate hash
+        require(salt > number);
+        require(generateHash(number, salt) == _tickets[msg.sender]);
+        winnerSeed = winnerSeed ^ salt ^ uint(msg.sender);
+        _verified.push(msg.sender);
+    }
+
+    function checkWinner()
+        public
+        returns (bool) {
+        // Within the timeframe
+        require(block.timestamp >= start + buyPeriod + verifyPeriod);
+        require(block.timestamp < start + buyPeriod + verifyPeriod + checkPeriod);
+        if (!hasWinner) {
+            winner = _verified[winnerSeed % _verified.length];
+            _winnings[winner] = _verified.length - 10 ether;
+            hasWinner = true;
+        }
+        return msg.sender == winner;
+    }
+
+    function claim()
+        public {
+        // Has winnings to claim
+        require(_winnings[msg.sender] > 0);
+        msg.sender.transfer(_winnings[msg.sender]);
+        _winnings[msg.sender] = 0;
+    }
 }
