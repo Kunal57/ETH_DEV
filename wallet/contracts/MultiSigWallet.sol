159a62bfbf69dbe492fc80e4e11e34d27a09e53e
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-23 12:33:51 -0600

    Write signTransaction & deleteTransaction functions

--- wallet/contracts/MultiSigWallet.sol
+++ wallet/contracts/MultiSigWallet.sol
@@ -30,8 +30,9 @@ contract MultiSigWallet {
     }
 
     event DepositFunds(address from, uint amount);
-    event WithdrawFunds(address to, uint amount);
-    event TransferFunds(address from, address to, uint amount);
+    event TransactionCreated(address from, address to, uint amount, uint transactionId);
+    event TransactionCompleted(address from, address to, uint amount, uint transactionId);
+    event TransactionSigned(address by, uint transactionId);
 
     function MultiSigWallet() public {
         _owner = msg.sender;
@@ -49,15 +50,67 @@ contract MultiSigWallet {
         DepositFunds(msg.sender, msg.value);
     }
 
-    function withdraw(uint amount) public validOwner {
-        require(address(this).balance >= amount);
-
-        WithdrawFunds(msg.sender, amount);
+    function withdraw(uint amount) public {
+        transferTo(msg.sender, amount);
     }
 
     function transferTo(address to, uint amount) public validOwner {
         require(address(this).balance >= amount);
+        uint transactionId = _transactionIdx++;
 
-        TransferFunds(msg.sender, to, amount);
+        Transaction memory transaction;
+        transaction.from = msg.sender;
+        transaction.to = to;
+        transaction.amount = amount;
+        transaction.signatureCount = 0;
+
+        _transactions[transactionId] = transaction;
+        _pendingTransactions.push(transactionId);
+
+        TransactionCreated(msg.sender, to, amount, transactionId);
     }
+
+    function getPendingTransactions() public validOwner returns (uint[]) {
+        return _pendingTransactions;
+    }
+
+    function signTransaction(uint transactionId) public validOwner {
+
+        Transaction storage transaction = _transactions[transactionId];
+
+
+        // Transaction must exist
+        require(0x0 != transaction.from);
+        // Creator cannot sign the transaction
+        require(msg.sender != transaction.from);
+        // Cannot sign a transaction more than once
+        require(transaction.signatures[msg.sender] != 1);
+
+        transaction.signatures[msg.sender] = 1;
+        transaction.signatureCount++;
+
+        TransactionSigned(msg.sender, transactionId);
+
+        if (transaction.signatureCount >= MIN_SIGNATURES) {
+            require(address(this).balance >= amount);
+            transaction.to.transfer(amount);
+            TransactionCompleted(transaction.from, transaction.to, transaction.amount, transactionId);
+            deleteTransaction(transactionId);
+        }
+    }
+
+    function deleteTransaction(uint transactionId) public validOwner {
+        uint8 replace = 0;
+        for (uint i = 0; i < _pendingTransactions.length; i++) {
+            if (1 == replace) {
+                _pendingTransactions[i - 1] = _pendingTransactions[i];
+            } else if (transactionId == _pendingTransactions[i]) {
+                replace = 1;
+            }
+        }
+        delete _pendingTransactions[_pendingTransactions.length - 1];
+        _pendingTransactions.length--;
+        delete _transaction[transactionId];
+    }
+
 }
