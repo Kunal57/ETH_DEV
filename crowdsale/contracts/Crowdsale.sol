d3001b52427aaf0678e238ecb2557c067d52694a
Kunal Patel <kunalpatelx92@gmail.com>
2017-12-22 18:28:32 -0600

    Add function to transfer 1000 tokens to the Crowdsale contract

--- crowdsale/contracts/Crowdsale.sol
+++ crowdsale/contracts/Crowdsale.sol
@@ -1,14 +1,18 @@
 pragma solidity ^0.4.17;
 
 import "./ERC223ReceivingContract.sol";
+import "./SafeMath.sol";
 
 contract Crowdsale is ERC223ReceivingContract {
+
+  using SafeMath for uint;
 
   address private _token;
   uint private _start;
   uint private _end;
   uint private _price;
   uint private _limit;
+  uint private _balance;
 
   function Crowdsale(address token, uint start, uint end, uint price, uint limit) {
     _token = token;
@@ -19,7 +23,7 @@ contract Crowdsale is ERC223ReceivingContract {
   }
 
   function availableBalance() public view returns(uint) {
-
+    return _balance;
   }
 
   function buy() public payable {
@@ -31,7 +35,7 @@ contract Crowdsale is ERC223ReceivingContract {
   }
 
   function tokenFallback(address _from, uint _value, bytes _data) public {
-
+    _balance = _balance.add(_value);
   }
 
 }
